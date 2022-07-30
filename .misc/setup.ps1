2275784ef391c01b8b3b27c3d45683229091d1f9
dundargoc <gocundar@gmail.com>
2022-07-30 14:05:30 +0200

    dotfiles: add setup instructions

---
+++ .misc/setup.ps1
@@ -0,0 +1,7 @@
+# Set up dotfiles.
+# NOTE: haven't tested if script actually works, the commands itself are correct though.
+cd ~
+git init
+git remote add origin git@github.com:dundargoc/dotfiles-windows.git
+git fetch
+git checkout -f master
