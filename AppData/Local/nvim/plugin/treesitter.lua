d2a02c01dc7c92e226bfd88b81fe8c1a68816291
dundargoc <gocdundar@gmail.com>
2022-09-04 18:21:14 +0200

    nvim(ts): don't install commonlisp and treesitter

--- AppData/Local/nvim/plugin/treesitter.lua
+++ AppData/Local/nvim/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
     ensure_installed = 'all',
-    ignore_install = { 'phpdoc', 'php', 'tlaplus' },
+    ignore_install = { 'phpdoc', 'php', 'tlaplus', 'llvm', 'commonlisp' },
     highlight = {
         enable = true,
     },
